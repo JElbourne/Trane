c3371d9c095078cd228ffc73dbe34aa0c9215353
JElbourne <jayelbourne@gmail.com>
2017-10-12 01:20:16 -0400

    Added the removal of directories that already exist, also added some better ux elements.

--- bin/index.js
+++ bin/index.js
@@ -1,26 +1,9 @@
 #!/usr/bin/env node
 
-const chalk = require('chalk');
-const clear = require('clear');
-const figlet = require('figlet');
 const yargs = require('yargs');
 
-// Initial setup of the terminal window
-// Clear screen
-clear();
-console.log(
-    // Change to yellow
-    chalk.yellow(
-        // Create ASCII logo for Trane
-        figlet.textSync('Trane', { horizontalLayout: 'full' })
-    )
-);
-
-// Get all the args from input using Yargs
-const argv = yargs.argv;
-const command = argv._[0];
-
 yargs.commandDir('../lib/cmds')
-    .demandCommand()
+    .demandCommand(1, 'You need at least one command before moving on')
+    .recommendCommands()
     .help()
     .argv
